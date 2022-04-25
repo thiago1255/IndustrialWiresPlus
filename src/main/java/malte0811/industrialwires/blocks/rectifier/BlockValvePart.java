7b1e325d27c1b53d19bda3abeea9e3b72bc3dab9
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-04-25 01:14:03 -0300

    Create BlockValvePart.java

---
+++ src/main/java/malte0811/industrialwires/blocks/rectifier/BlockValvePart.java
@@ -0,0 +1,7 @@
+/*
+* Tile Entity file for the rectifier valve.
+* File made by thiago1255.
+* Free to use, but check other files/libraries permission first.
+*/
+
+package malte0811.industrialwires.rectifier;
