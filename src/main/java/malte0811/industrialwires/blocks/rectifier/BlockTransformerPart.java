b2b2b8479cb537719bf3fdedf33079cd050eff0e
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-04-26 03:15:04 -0300

    Update BlockTransformerPart.java

--- src/main/java/malte0811/industrialwires/blocks/rectifier/BlockTransformerPart.java
+++ src/main/java/malte0811/industrialwires/blocks/rectifier/BlockTransformerPart.java
@@ -5,3 +5,18 @@
 */
 
 package malte0811.industrialwires.rectifier;
+
+import blusunrize.immersiveengineering.api.IEProperties;
+import malte0811.industrialwires.blocks.BlockIWBase;
+import malte0811.industrialwires.blocks.IMetaEnum;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
