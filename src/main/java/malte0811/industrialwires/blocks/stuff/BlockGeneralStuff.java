c93c1504b4c3f05ae5433b6677cd5f88c49fe68f
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-05-08 17:52:20 -0300

    fixed again PROPERTY

--- src/main/java/malte0811/industrialwires/blocks/stuff/BlockGeneralStuff.java
+++ src/main/java/malte0811/industrialwires/blocks/stuff/BlockGeneralStuff.java
@@ -29,8 +29,8 @@ public class BlockGeneralStuff extends BlockIWBase implements IMetaEnum {
 	}
 
         @Override
-	protected IProperty<?>[] getProperties() {
-		return new IProperty[] {IEProperties.FACING_HORIZONTAL, type};
+	protected IProperty[] getProperties() {
+		return new IProperty[] {PROPERTY, IEProperties.FACING_HORIZONTAL};
 	}
 
 	@Override
