5facbcb84ed45a75ebb4151d3297f629c70ffbdf
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-05-15 03:32:29 -0300

    Update TileEntityControlTransformer.java

--- src/main/java/malte0811/industrialwires/blocks/stuff/TileEntityControlTransformer.java
+++ src/main/java/malte0811/industrialwires/blocks/stuff/TileEntityControlTransformer.java
@@ -14,22 +14,24 @@ import javax.annotation.Nonnull;
 public class TileEntityControlTransformer extends TileEntityIWBase implements IPlayerInteraction, IMarxTarget,
 		IBlockBoundsDirectional, IDirectionalTile 
 {  
+          private int dummy = 0;
 	  private static final String FACING = "facing";
 	  EnumFacing facing = EnumFacing.NORTH;
 
 	  @Override
 	  public void writeNBT(NBTTagCompound out, boolean updatePacket) {
+                nbt.setInteger("dummy", dummy);
 	   	out.setByte(FACING, (byte) facing.getHorizontalIndex());
   	}
 
   	@Override
 	  public void readNBT(NBTTagCompound in, boolean updatePacket) {;
+                dummy = nbt.getInteger("dummy");
 	  	facing = EnumFacing.byHorizontalIndex(in.getByte(FACING));
 	  	aabb = null;
    	}
 
-
-  //NEED FINISH UNDER
+//NEED FINISH UNDER:
 	AxisAlignedBB aabb = null;
 	@Override
 	public AxisAlignedBB getBoundingBoxNoRot() {
@@ -44,35 +46,61 @@ public class TileEntityControlTransformer extends TileEntityIWBase implements IP
 		}
 		return aabb;
 	}
-        //ok
+//FINISHED UNDER THIS:
 	@Nonnull
 	@Override
 	public EnumFacing getFacing() {
 		return facing;
 	}
-        //ok
+        
 	@Override
 	public void setFacing(@Nonnull EnumFacing facing) {
 		this.facing = facing;
 	}
-        //ok
+        
 	@Override
 	public int getFacingLimitation() {
 		return 2;
 	}
-        //ok ?
+        
 	@Override
 	public boolean mirrorFacingOnPlacement(@Nonnull EntityLivingBase placer) {
 		return false;
 	}
-        //ok ?
+        
 	@Override
 	public boolean canHammerRotate(@Nonnull EnumFacing side, float hitX, float hitY, float hitZ, @Nonnull EntityLivingBase entity) {
 		return false;
 	}
-        //ok
+        
 	@Override
 	public boolean canRotate(@Nonnull EnumFacing axis) {
 		return false;
 	}
+        
+        @Override
+	public boolean isDummy()
+	{
+		return dummy!=0;
+	}
+
+        @Override
+	public void placeDummies(BlockPos pos, IBlockState state, EnumFacing side, float hitX, float hitY, float hitZ)
+	{
+			for(int i = 1; i <= 1; i++)
+			{
+				world.setBlockState(pos.add(0, 0, i), state);
+				((TileEntityControlTransformer)world.getTileEntity(pos.add(0, 0, i))).dummy = i;
+				((TileEntityControlTransformer)world.getTileEntity(pos.add(0, 0, i))).facing = this.facing;
+			}
+	}
+        @Override
+	public void breakDummies(BlockPos pos, IBlockState state)
+	{
+		for(int i = 0; i <= 1; i++)
+                {
+		       world.setBlockToAir(getPos().add(0, 0, -dummy).add(0, 0, i));
+                }
+	}
+
 }
