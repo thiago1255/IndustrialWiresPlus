b2bd6d098b042cb08ad450b747bb9c0652fb9bad
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-05-16 04:07:35 -0300

    Update TileEntityControlTransformer.java

--- src/main/java/malte0811/industrialwires/blocks/stuff/TileEntityControlTransformer.java
+++ src/main/java/malte0811/industrialwires/blocks/stuff/TileEntityControlTransformer.java
@@ -3,7 +3,7 @@ package malte0811.industrialwires.blocks.stuff;
 import blusunrize.immersiveengineering.api.ApiUtils;
 import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IDirectionalTile;
 import malte0811.industrialwires.blocks.IBlockBoundsIW;
-import malte0811.industrialwires.blocks.TileEntityIWBase;
+import blusunrize.immersiveengineering.common.blocks.TileEntityIEBase;
 import malte0811.industrialwires.blocks.IHasDummyBlocksIW;
 import malte0811.industrialwires.blocks.IWProperties;
 import net.minecraft.entity.EntityLivingBase;
@@ -17,7 +17,7 @@ import net.minecraft.util.math.AxisAlignedBB;
 
 import javax.annotation.Nonnull;
 
-public class TileEntityControlTransformer extends TileEntityIWBase implements IHasDummyBlocksIW, IBlockBoundsIW, IDirectionalTile 
+public class TileEntityControlTransformer extends TileEntityIEBase implements IHasDummyBlocksIW, IBlockBoundsIW, IDirectionalTile 
 {  
           private int dummy = 0;
 	  private static final String FACING = "facing";
