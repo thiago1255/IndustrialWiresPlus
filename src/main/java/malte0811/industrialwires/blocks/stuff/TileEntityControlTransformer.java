711917dbe74e416166c71f5932edf12368223981
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-05-17 02:13:31 -0300

    Update TileEntityControlTransformer.java

--- src/main/java/malte0811/industrialwires/blocks/stuff/TileEntityControlTransformer.java
+++ src/main/java/malte0811/industrialwires/blocks/stuff/TileEntityControlTransformer.java
@@ -2,47 +2,49 @@ package malte0811.industrialwires.blocks.stuff;
 
 import blusunrize.immersiveengineering.api.ApiUtils;
 import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IDirectionalTile;
-import malte0811.industrialwires.blocks.IBlockBoundsIW;
-import blusunrize.immersiveengineering.common.blocks.TileEntityIEBase;
-import malte0811.industrialwires.blocks.IHasDummyBlocksIW;
-import malte0811.industrialwires.blocks.IWProperties;
+import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IPlayerInteraction;
+import malte0811.industrialwires.IndustrialWires;
+import malte0811.industrialwires.blocks.IBlockBoundsIW.IBlockBoundsDirectional;
+import malte0811.industrialwires.blocks.TileEntityIWBase;
+import malte0811.industrialwires.hv.IMarxTarget;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
-import net.minecraft.block.Block;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.text.TextComponentTranslation;
 
 import javax.annotation.Nonnull;
 
-public class TileEntityControlTransformer extends TileEntityIEBase implements IHasDummyBlocksIW, IBlockBoundsIW, IDirectionalTile 
-{  
-          private int dummy = 0;
-	  private static final String FACING = "facing";
-	  EnumFacing facing = EnumFacing.NORTH;
-
-        @Override
-	public void readCustomNBT(@Nonnull NBTTagCompound nbt, boolean descPacket) {
-                dummy = nbt.getInteger("dummy");
-	  	facing = EnumFacing.HORIZONTALS[nbt.getInteger("facing")];
-   	}
+public class TileEntityControlTransformer extends TileEntityIWBase implements IBlockBoundsDirectional, IDirectionalTile {
+	private static final String FACING = "facing";
+	EnumFacing facing = EnumFacing.NORTH;
 
 	@Override
-	public void writeCustomNBT(@Nonnull NBTTagCompound nbt, boolean descPacket) {
-                nbt.setInteger("dummy", dummy);
-	   	nbt.setInteger("facing", facing.getHorizontalIndex());
-  	}
+	public void writeNBT(NBTTagCompound out, boolean updatePacket) {
+		out.setByte(FACING, (byte) facing.getHorizontalIndex());
+	}
+
+	@Override
+	public void readNBT(NBTTagCompound in, boolean updatePacket) {
+		facing = EnumFacing.byHorizontalIndex(in.getByte(FACING));
+		aabb = null;
+	}
+
+	AxisAlignedBB aabb = null;
+	@Override
+	public AxisAlignedBB getBoundingBoxNoRot() {
+		return new AxisAlignedBB(0, 0, 0, 16, 32, 16);
+	}
 
 	@Override
 	public AxisAlignedBB getBoundingBox() {
-		if (!isDummy()) {
-			return Block.FULL_BLOCK_AABB;
-		} else {
-                        return Block.FULL_BLOCK_AABB;
-                }
+		if (aabb==null) {
+			aabb = IBlockBoundsDirectional.super.getBoundingBox();
+		}
+		return aabb;
 	}
 
 	@Nonnull
@@ -50,55 +52,29 @@ public class TileEntityControlTransformer extends TileEntityIEBase implements IH
 	public EnumFacing getFacing() {
 		return facing;
 	}
-        
+
 	@Override
 	public void setFacing(@Nonnull EnumFacing facing) {
 		this.facing = facing;
 	}
-        
+
 	@Override
 	public int getFacingLimitation() {
 		return 2;
 	}
-        
+
 	@Override
 	public boolean mirrorFacingOnPlacement(@Nonnull EntityLivingBase placer) {
 		return false;
 	}
-        
+
 	@Override
 	public boolean canHammerRotate(@Nonnull EnumFacing side, float hitX, float hitY, float hitZ, @Nonnull EntityLivingBase entity) {
 		return false;
 	}
-        
+
 	@Override
 	public boolean canRotate(@Nonnull EnumFacing axis) {
 		return false;
 	}
-        
-        @Override
-	public boolean isDummy()
-	{
-		return dummy!=0;
-	}
-
-        @Override
-	public void placeDummies(IBlockState state)
-	{
-			for(int i = 1; i <= 1; i++)
-			{
-				world.setBlockState(pos.add(0, 0, i), state);
-				((TileEntityControlTransformer)world.getTileEntity(pos.add(0, 0, i))).dummy = i;
-				((TileEntityControlTransformer)world.getTileEntity(pos.add(0, 0, i))).facing = this.facing;
-			}
-	}
-        @Override
-	public void breakDummies()
-	{
-		for(int i = 0; i <= 1; i++)
-                {
-		       world.setBlockToAir(pos.offset(EnumFacing.WEST, i - dummy));
-                }
-	}
-
 }
