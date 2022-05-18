bd1ba27a422c4d5129d6ee00d171a4aa78b4fbe4
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-05-18 01:55:57 -0300

    Update BlockTypes_GeneralStuff.java

--- src/main/java/malte0811/industrialwires/blocks/stuff/BlockTypes_GeneralStuff.java
+++ src/main/java/malte0811/industrialwires/blocks/stuff/BlockTypes_GeneralStuff.java
@@ -4,13 +4,29 @@ Made using other files of this mod as example/model.
 
 package malte0811.industrialwires.blocks.stuff;
 
+import blusunrize.immersiveengineering.common.blocks.BlockIEBase;
 import net.minecraft.util.IStringSerializable;
 
-public enum BlockTypes_GeneralStuff implements IStringSerializable{
+import java.util.Locale;
+
+public enum BlockTypes_GeneralStuff implements IStringSerializable, BlockIEBase.IBlockEnum{
 	CONTROL_TRANSFORMER;
 
 	@Override
-	public String getName() {
+	public String getName() 
+        {
 		return name().toLowerCase();
+	}
+
+	@Override
+	public int getMeta()
+        {
+		return ordinal();
+	}
+
+	@Override
+	public boolean listForCreative() 
+        {
+		return true;
 	}
 }
