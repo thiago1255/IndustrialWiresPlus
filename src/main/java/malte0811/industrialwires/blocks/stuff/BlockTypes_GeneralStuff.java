9266b68e05a366e3d805603b8caadb553962ef2d
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-05-18 03:17:23 -0300

    Add files via upload

---
+++ src/main/java/malte0811/industrialwires/blocks/stuff/BlockTypes_GeneralStuff.java
@@ -0,0 +1,16 @@
+/*
+Made using other files of this mod as example/model.
+*/
+
+package malte0811.industrialwires.blocks.stuff;
+
+import net.minecraft.util.IStringSerializable;
+
+public enum BlockTypes_GeneralStuff implements IStringSerializable{
+	CONTROL_TRANSFORMER;
+
+	@Override
+	public String getName() {
+		return name().toLowerCase();
+	}
+}
