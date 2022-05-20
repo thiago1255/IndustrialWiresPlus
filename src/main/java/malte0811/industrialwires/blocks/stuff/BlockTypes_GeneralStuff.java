479aca519eba4a1f7378dab6dd52ed80dde0b9db
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-05-20 04:15:20 -0300

    maybe now all blocks will be registered

--- src/main/java/malte0811/industrialwires/blocks/stuff/BlockTypes_GeneralStuff.java
+++ src/main/java/malte0811/industrialwires/blocks/stuff/BlockTypes_GeneralStuff.java
@@ -4,14 +4,24 @@ Made using other files of this mod as example/model.
 
 package malte0811.industrialwires.blocks.stuff;
 
-import net.minecraft.util.IStringSerializable;
+import blusunrize.immersiveengineering.common.blocks.BlockIEBase.IBlockEnum;
 
-public enum BlockTypes_GeneralStuff implements IStringSerializable{
+public enum BlockTypes_GeneralStuff implements IBlockEnum{
 	CONTROL_TRANSFORMER,
         VARISTOR;
 
 	@Override
 	public String getName() {
 		return name().toLowerCase();
+	}
+        
+        @Override
+	public int getMeta() {
+		return ordinal();
+	}
+
+	@Override
+	public boolean listForCreative() {
+		return true;
 	}
 }
