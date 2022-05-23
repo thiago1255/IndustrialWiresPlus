660a1f3e77f95dfeced2f10ca94f7b9b44c06594
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-05-23 03:17:25 -0300

    1.8-10

--- src/main/java/malte0811/industrialwires/blocks/stuff/TileEntityVaristor.java
+++ src/main/java/malte0811/industrialwires/blocks/stuff/TileEntityVaristor.java
@@ -1,3 +1,12 @@
+/*
+|| (do what u want with this, but give credits to:)
+|| File made by thiago1255 based (copied a lot) of files of mods 'Industrial Wires', and 'Immersive Engineering'.
+||
+|| (check github for credits of this mods:)
+|| IW: https://github.com/malte0811/IndustrialWires
+|| IE: https://github.com/BluSunrize/ImmersiveEngineering
+*/
+
 package malte0811.industrialwires.blocks.stuff;
 
 import blusunrize.immersiveengineering.api.ApiUtils;
