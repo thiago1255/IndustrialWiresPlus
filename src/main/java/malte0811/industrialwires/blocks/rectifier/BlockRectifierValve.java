30492d6d149ff6d58ca1cbb9bb0edc334ab80eae
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-06-02 01:18:03 -0300

    1.8-19 (deleted retifier valve classes, will be included in Stuff on 1.10 or 2.0)

--- src/main/java/malte0811/industrialwires/blocks/rectifier/BlockRectifierValve.java
+++
@@ -1,7 +0,0 @@
-/*
-* Tile Entity file for the rectifier valve.
-* File made by thiago1255.
-* Free to use, but check other files/libraries permission first.
-*/
-
-package malte0811.industrialwires.rectifier;
