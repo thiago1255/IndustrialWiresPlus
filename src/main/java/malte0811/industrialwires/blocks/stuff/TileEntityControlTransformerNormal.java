a94abf10ebb26960fa81b8799859b8d11462c129
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-06-08 02:17:30 -0300

    1.8-21 (start new TEs)

---
+++ src/main/java/malte0811/industrialwires/blocks/stuff/TileEntityControlTransformerNormal.java
@@ -0,0 +1,14 @@
+/*
+|| UNDER 'GNU General Public License v3.0'
+|| File made by thiago1255 based (copied a lot) of files of mods 'Industrial Wires', and 'Immersive Engineering'.
+||
+|| (check github for credits of this mods:)
+|| IW: https://github.com/malte0811/IndustrialWires
+|| IE: https://github.com/BluSunrize/ImmersiveEngineering
+*/
+
+package malte0811.industrialwires.blocks.stuff;
+
+public class TileEntityControlTransformerNormal extends TileEntityImmersiveConnectable implements ITickable, IIEInternalFluxHandler, IBlockBoundsDirectional, IDirectionalTile  
+{
+}
