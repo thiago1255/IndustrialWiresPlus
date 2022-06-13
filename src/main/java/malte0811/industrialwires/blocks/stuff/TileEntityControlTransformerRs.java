d6c72b4caaa09542267e247b1b64ca84697dcee4
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2022-06-13 19:24:36 -0300

    1.8-25 (fixed again)

--- src/main/java/malte0811/industrialwires/blocks/stuff/TileEntityControlTransformerRs.java
+++ src/main/java/malte0811/industrialwires/blocks/stuff/TileEntityControlTransformerRs.java
@@ -26,6 +26,7 @@ import blusunrize.immersiveengineering.common.util.EnergyHelper;
 import blusunrize.immersiveengineering.common.util.EnergyHelper.IEForgeEnergyWrapper;
 import blusunrize.immersiveengineering.common.util.EnergyHelper.IIEInternalFluxHandler;
 import blusunrize.immersiveengineering.common.util.Utils;
+import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.*;
 import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IDirectionalTile;
 import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IPlayerInteraction;
 
@@ -64,7 +65,7 @@ import static blusunrize.immersiveengineering.api.energy.wires.WireType.MV_CATEG
 import static blusunrize.immersiveengineering.api.energy.wires.WireType.HV_CATEGORY;
 import static blusunrize.immersiveengineering.api.energy.wires.WireType.REDSTONE_CATEGORY;
 
-public class TileEntityControlTransformerRs extends TileEntityImmersiveConnectable implements ITickable, IIEInternalFluxHandler, IPlayerInteraction, IBlockBoundsDirectional, IDirectionalTile, IRedstoneConnector  
+public class TileEntityControlTransformerRs extends TileEntityImmersiveConnectable implements ITickable,  IHammerInteraction, IIEInternalFluxHandler, IPlayerInteraction, IBlockBoundsDirectional, IDirectionalTile, IRedstoneConnector  
 {
 // VARIABLES/CONS.: --------------------------------------
     private static final String SOUTH = "south";
@@ -240,6 +241,11 @@ public class TileEntityControlTransformerRs extends TileEntityImmersiveConnectab
     public void updateInput(byte[] signals) {
     }
 
+    @Override
+    public World getConnectorWorld() {
+        return getWorld();
+    }
+	
 // GENERAL PROPERTYES: --------------------------------------       
     @Override
     public boolean hammerUseSide(EnumFacing side, EntityPlayer player, float hitX, float hitY, float hitZ) {
